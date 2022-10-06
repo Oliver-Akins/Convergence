a9276248992ebfa964716a1637791200d4a7a199
Oliver Akins <oliver.akins1999@gmail.com>
2022-10-06 15:45:21 -0600

    Add DB interaction functions for accounts

--- server/src/utils/database/json.ts
+++ server/src/utils/database/json.ts
@@ -1,4 +1,5 @@
 import { databaseOptions } from "$/types/config";
+import { randomString } from "@hapi/cryptiles";
 import { Account } from "$/types/data";
 import fs from "fs";
 
@@ -45,5 +46,55 @@ export class JSONDatabase {
 
 	public async shutdown() {
 		fs.writeFileSync(this.conf.uri, JSON.stringify(this.data));
+	};
+
+	/**
+	 * Retrieves the secret used for hashing passwords
+	 */
+	public getHashSecret(): string {
+		if (!this.data.meta[`hash-secret`]) {
+			this.data.meta[`hash-secret`] = randomString(15);
+		};
+		return this.data.meta[`hash-secret`];
+	};
+
+	/**
+	 * Retrieves a user's account from the database provided their username.
+	 *
+	 * @param username The username of the account
+	 * @returns The accounts that have that username
+	 */
+	public getAccountsByUsername(username: string): Account[] {
+		let users: Account[] = [];
+		for (const aid in this.data.users) {
+			const account = this.data.users[aid];
+			if (username == account.username) {
+				users.push(account);
+			};
+		};
+		return users;
+	};
+
+	public getAccountByUsernameDiscriminator(
+		username: string,
+		discriminator: number
+	): Account | undefined {
+		for (const aid in this.data.users) {
+			const account = this.data.users[aid];
+			if (
+				username == account.username
+				&& discriminator == account.discriminator
+			) {
+				return account;
+			};
+		};
+	};
+
+	/**
+	 * Adds the user into the database.
+	 */
+	public addUser(user: Account) {
+		this.data.users[++this.data.meta.users.index] = user;
+		this.data.meta.users.count++;
 	};
 };
