68a3726cc8518da865a4db2d1aaf482baf7a8347
Oliver <oliver.akins1999@gmail.com>
2022-10-08 10:20:27 -0600

    Add React starter files (#4)
    Co-authored-by: Andy Tran <atran@celtx.com>
    Co-authored-by: Andy Tran <foolmonkey99@gmail.com>

---
+++ site/src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+      <main>
+        <h1 className="heading">Convergence</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
