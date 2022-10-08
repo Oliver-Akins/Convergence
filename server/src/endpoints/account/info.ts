492a0268cac25206a1938bf49aed46a0caf78703
Oliver Akins <oliver.akins1999@gmail.com>
2022-10-08 14:54:43 -0600

    Implement the API endpoint for getting account info

---
+++ server/src/endpoints/account/info.ts
@@ -0,0 +1,48 @@
+import { usernameRegEx, discriminatorRegEx } from "$/schemas/accounts";
+import { cleanAccount } from "$/utils/data_cleaner";
+import { ServerRoute } from "@hapi/hapi";
+import { Account } from "$/types/data";
+import { database } from "$/main";
+import boom from "@hapi/boom";
+import Joi from "joi";
+
+const route: ServerRoute = {
+	method: `GET`, path: `/users/{user}`,
+	options: {
+		validate: {
+			params: Joi.object({
+				user: Joi
+					.string()
+					.pattern(new RegExp(`^${usernameRegEx}\.${discriminatorRegEx}$`))
+					.allow(`@me`),
+			}),
+		},
+	},
+	async handler(req, h) {
+		const { user } = req.params;
+		const authed = req.auth.credentials as unknown as Account;
+
+		if (
+			user == `@me`
+			|| user == `${authed.username}.${authed.discriminator}`
+		) {
+			return h.response(cleanAccount(authed, false)).code(200);
+		};
+
+		let [ name, discrim ] = user.split(`.`) as [string, any];
+		try {
+			discrim = parseInt(discrim);
+		} catch (_) {
+			throw boom.badRequest(`Invalid discriminator`);
+		};
+
+		let account = await database.getAccountByUsernameDiscriminator(name, discrim);
+
+		if (!account) {
+			throw boom.notFound(`No account found with that username/discrim`);
+		};
+
+		return h.response(cleanAccount(account)).code(206);
+	},
+};
+export default route;
