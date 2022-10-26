3f7cae1e75d75094b43cef4133223ef2ed29cb73
Oliver Akins <oliver.akins1999@gmail.com>
2022-10-26 17:48:57 -0600

    Remove authorization requirement the internal endpoint

--- server/src/endpoints/internal/reset_db.ts
+++ server/src/endpoints/internal/reset_db.ts
@@ -5,9 +5,9 @@ const route: ServerRoute = {
 	method: `GET`, path: `/internal/db-reset`,
 	options: {
 		isInternal: true,
+		auth: false,
 	},
 	async handler(_, h) {
-		console.log(`Resetting DB`)
 		await database.reset();
 		return h.response().code(200);
 	},
