8419bae263fa0fed21741a1c97c82046a1a817ab
Andy Tran <atran@celtx.com>
2022-10-26 16:32:58 -0600

    Add basic frontend routes

--- site/src/App.js
+++ site/src/App.js
@@ -2,13 +2,27 @@ import { Routes, Route, Link } from "react-router-dom";
 import './styles/App.scss';
 
 import Home from './pages/Home.tsx';
+import Register from './pages/Register.tsx';
+import Login from './pages/Login.tsx';
+import Dashboard from './pages/Dashboard.tsx';
+import NotFound from './pages/NotFound.tsx';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Sign Up</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ site/src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <header>
+      </header>
+      <main className="login">
+        <h1 className="heading">Login</h1>
+      </main>
+    </>
+  );
+}
+
+export default Home;
