b233d71c4a2d770795de9ef433fa75ba627f420f
Andy Tran <atran@celtx.com>
2022-11-06 17:46:16 -0600

    Add Login and Register pages with styling
    Basic styling and stubbed functionality

--- site/src/pages/Login.tsx
+++ site/src/pages/Login.tsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../components/Button";
+
 
 function Home() {
   return (
     <>
       <header>
       </header>
-      <main className="login">
-        <h1 className="heading">Login</h1>
+      <main>
+        <section className="card card--login">
+          <h1 className="header">Convergence</h1>
+          <Button text="Continue with Discord" classes="btn--grey" iconSrc="icons/discord.svg" onClickCallback={()=>{alert("discord")}} />
+          <Button text="Continue with Steam" classes="btn--grey" iconSrc="icons/steam.svg" onClickCallback={()=>{alert("steam")}} />
+          <Button text="Continue with Itch.io"classes="btn--grey" iconSrc="icons/itch-io.svg" onClickCallback={()=>{alert("itch.io")}} />
+          <span className="divider">
+            <p className="caps">Or</p>
+          </span>
+          <div className="login__form">
+            <label className="caps" htmlFor="username">Username</label>
+            <input type="text" id="username"></input>
+            <label className="caps" htmlFor="password">Password</label>
+            <input type="password" id="password"></input>
+            <Button text="Login" classes="btn btn--form"/>
+          </div>
+          <div>
+            <p>Don't have an account? <Link to="/register">Register</Link></p>
+          </div>
+        </section>
       </main>
     </>
   );
--- site/src/pages/Register.tsx
+++ site/src/pages/Register.tsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../components/Button";
+
 
 function Home() {
   return (
     <>
       <header>
       </header>
-      <main className="register">
-        <h1 className="heading">Register</h1>
+      <main>
+        <section className="card card--register">
+          <h1 className="header">Create an account</h1>
+          <Button text="Continue with Discord" classes="btn--grey" iconSrc="icons/discord.svg" onClickCallback={()=>{alert("discord")}} />
+          <Button text="Continue with Steam" classes="btn--grey" iconSrc="icons/steam.svg" onClickCallback={()=>{alert("steam")}} />
+          <Button text="Continue with Itch.io"classes="btn--grey" iconSrc="icons/itch-io.svg" onClickCallback={()=>{alert("itch.io")}} />
+          <span className="divider">
+            <p className="caps">Or</p>
+          </span>
+          <div className="register__form">
+            <label className="caps" htmlFor="email">Email Address</label>
+            <input type="email" id="email"></input>
+            <label className="caps" htmlFor="username">Username</label>
+            <input type="text" id="username"></input>
+            <label className="caps" htmlFor="password">Password</label>
+            <input type="password" id="password"></input>
+            <Button text="Register" classes="btn btn--form"/>
+          </div>
+          <p>Already have an account? <Link to="/login">Login</Link></p>
+        </section>
       </main>
     </>
   );
