ba00d34522d674875a6f5e8f5b6c073101d06222
Andy Tran <atran@celtx.com>
2022-11-10 16:54:21 -0600

    Add shared games header in dashboard

--- site/src/components/GameComparison.jsx
+++ site/src/components/GameComparison.jsx
@@ -4,11 +4,19 @@ import Game from "./Game";
 function GameComparison({ games=null }) {
     return (
         <section className="game-comparison">
-            {games ? games.map((game, i) => {
-                    return (
-                        <Game game={game} key={i} />
-                    );
-            }):<p className="get-started-text">Select a Friend to Begin!</p>}
+            {games ? 
+                <>
+                    <div className="game-comparison__heading">
+                        <h2>Shared Games</h2>
+                        <p className="games-number">{ games.length } in common</p>
+                    </div>
+                    {games.map((game, i) => {
+                        return (
+                            <Game game={game} key={i} />
+                        );
+                    })}
+                </>
+            :<p className="get-started-text">Select a Friend to Begin!</p>}
         </section>
     );
 }
