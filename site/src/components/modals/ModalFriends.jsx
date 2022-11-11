57326f782fdb3a8fde3e1b232593e060d6edde01
Andy Tran <atran@celtx.com>
2022-11-10 21:20:49 -0600

    Add v1 of friends modal

---
+++ site/src/components/modals/ModalFriends.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Button } from "../Button";
+import Modal from "../Modal";
+import Person from "../Person";
+
+let friendsListFake = [
+    {
+      "username": "User1",
+      "id": "0001",
+    },
+    {
+        "username": "User2",
+        "id": "2324",
+    }
+  ];
+
+function ModalFriends() {
+    const HamburgerMenu = () => {
+        return (
+            <button className="btn-icon btn-icon--hamburger">
+                <img src={require("../../images/icons/hamburger.svg").default} alt="Menu"></img> 
+            </button>
+        );
+    }
+
+    const CompareLibraryButton = () => {
+        return (
+            <Button text="Compare Library"/>
+        );
+    }
+
+    function FriendsList({friendsList}) {
+        return (
+            <div className="friends-list">
+                {friendsList.map((friend, i) => {
+                    return (
+                        <Person person={friend} classes="person--manage" key={i} buttons={[CompareLibraryButton, HamburgerMenu]}/>
+                    );
+                })}
+            </div>
+        );
+    }
+
+    function AddFriendSection() {
+        return (
+            <div className="add-friend">
+                <label className="small-caps" htmlFor="username">Add Friend</label>
+                <div className="add-friend__inputs">
+                    <input type="text" id="username" placeholder="Username#0001"></input>
+                    <Button text="Send Friend Request" />
+                </div>
+            </div>
+        );
+    }
+
+    function ModalContent() {
+        return (
+            <>
+                <h2 className="modal__header">Friends</h2>
+                <FriendsList friendsList={friendsListFake} />
+                <AddFriendSection />
+            </>
+        );
+    }
+
+    return (
+        <Modal children={[<ModalContent />]}/>
+    )
+}
+
+export default ModalFriends;
