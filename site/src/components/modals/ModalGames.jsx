5c30ebf078f7f93f907a48b0618cb88f26f268a3
Andy Tran <atran@celtx.com>
2022-11-12 14:10:07 -0600

    Update platform select options on game modal

--- site/src/components/modals/ModalGames.jsx
+++ site/src/components/modals/ModalGames.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { Button } from "../Button";
 import Modal from "../Modal";
+import Select from 'react-select';
+
+const platformOptions = [
+    { value: 'steam', label: 'Steam' },
+    { value:  'swtich', label: 'Nintendo Switch' },
+    { value: 'epic', label: 'Epic Games Store' },
+    { value: 'gog', label: 'GOG' },
+];
 
 function ModalAddGame({ setOpen }) {
     function ModalContent() {
@@ -9,8 +17,12 @@ function ModalAddGame({ setOpen }) {
                 <h2 className="modal__header">Add New Game</h2>
                 <div className="card modal__form">
                     <div className="modal__inputs">
-                        <label className="small-caps" htmlFor="addGame">Game Title</label>
-                        <input type="text" id="addGame"></input>
+                        <label className="small-caps" htmlFor="game-title">Game Title</label>
+                        <input type="text" id="game-title"></input>
+                        <label className="small-caps" htmlFor="game-publisher">Publisher</label>
+                        <input type="text" id="game-publisher"></input>
+                        <label className="small-caps" htmlFor="platforms">Platforms</label>
+                        <Select className="select" isMulti name="platforms" options={platformOptions} />
                     </div>
                 </div>
                 <div className="modal__controls">
