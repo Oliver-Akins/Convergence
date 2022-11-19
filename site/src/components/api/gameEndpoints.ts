9e2a4eeb63174f44b606a5cf1744ad644da0e641
Andy Tran <atran@celtx.com>
2022-11-18 21:58:35 -0600

    Fix game add and search endpoints

--- site/src/components/api/gameEndpoints.ts
+++ site/src/components/api/gameEndpoints.ts
@@ -1,21 +1,33 @@
 // TODO
-async function getGameSearch(searchQuery : string): Promise<any> {     
-    await fetch(`games/search?` + new URLSearchParams({
+async function getGameSearch(searchQuery : string): Promise<any> { 
+    const url = `games/search?` + new URLSearchParams({
         "query": `${searchQuery}`,
-    }), {
+    });
+    console.log(url);
+    
+
+    await fetch(url, {
         method: "GET",
     })
-        .then((response) => {
+        .then((response) => {            
             if(response.ok) return response.json();
+        }).then((res) => {
+            localStorage.setItem("gameSearch", JSON.stringify(res));
         })
-        .catch((err) => console.error(err));
+        .catch((err) => {
+            localStorage.removeItem("gameSearch");
+            console.error(err);
+        });
 }
 
-// TODO figure this out, also has a route for PUT and POST which all operate the same
-async function addGames(aUsername: string, games: object): Promise<any> {     
+// TODO figure this out, also has a route for PATCH and POST which all operate the same
+async function addGames(aUsername: string, games: object): Promise<any> {      
     await fetch(`users/${aUsername}/games`, {
         method: "PATCH",
-        body: JSON.stringify({ games }),
+        body: JSON.stringify(games),
+        headers: {
+            'Content-Type': 'application/json'
+        },
     })
         .then((response) => {
             if(response.ok) return response.json();
