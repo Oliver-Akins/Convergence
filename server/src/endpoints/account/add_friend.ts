8a4a48d7c3804e0ff4590c3a34325d5f1d4bd7e4
Oliver Akins <oliver.akins1999@gmail.com>
2022-11-19 14:25:17 -0600

    Update the add friends endpoint to accept a mixed array of u+d combos and user IDs

--- server/src/endpoints/account/add_friend.ts
+++ server/src/endpoints/account/add_friend.ts
@@ -1,42 +1,70 @@
+import { usernameDiscrimComboSchema } from "$/schemas/accounts";
 import { ServerRoute } from "@hapi/hapi";
 import { Account } from "$/types/data";
-import { database } from "$/main";
+import { database, log } from "$/main";
 import Joi from "joi";
 
 const route: ServerRoute = {
 	method: [`PUT`, `POST`], path: `/users/@me/friends`,
 	options: {
 		validate: {
-			payload: Joi.array().items(Joi.string().uuid()),
+			payload: Joi.array().items(
+				Joi.alternatives(
+					Joi.string().uuid(),
+					usernameDiscrimComboSchema
+				)
+			),
 		},
 	},
 	async handler(req, h) {
 		const users = req.payload as string[];
 		const authed = req.auth.credentials as unknown as Account;
+		log.info(`Attempting to add ${users.length} friends for ${authed.username}#${authed.discriminator}`);
 
 		let accepted: string[] = [];
 		let sent: string[] = [];
 		let errored: string[] = [];
 
 		for (const user of users) {
-			if (authed.relations.friends.includes(user)) {
-				errored.push(user);
+
+			let uid = user;
+			let other = null;
+
+
+			// It was a username provided, try to convert it to an user ID so
+			// that we have it exact
+			if (user.includes(`#`)) {
+				log.debug(`Trying to parse out the username/discrim`)
+				let [ username, discrim ] = user.split(`#`) as [string, any];
+				try {
+					discrim = parseInt(discrim);
+				} catch (e) {
+					errored.push(user);
+				};
+				other = await database.getAccountByUsernameDiscriminator(username, discrim);
+				if (other) {
+					uid = other.id;
+				};
+			};
+
+			if (authed.relations.friends.includes(uid)) {
+				errored.push(uid);
 			}
-			else if (authed.relations.requests.includes(user)) {
-				await database.acceptFriendRequest(authed.id, user);
-				accepted.push(user);
+			else if (authed.relations.requests.includes(uid)) {
+				await database.acceptFriendRequest(authed.id, uid);
+				accepted.push(uid);
 			}
 			else {
-				let other = await database.getAccountByID(user);
 				if (!other) {
-					errored.push(user);
-				}
-				else if (other.relations.blocked.includes(authed.id)) {
-					errored.push(user);
+					other = await database.getAccountByID(uid);
+				};
+
+				if (!other || other.relations.blocked.includes(authed.id)) {
+					errored.push(uid);
 				}
 				else {
-					await database.sendFriendRequest(authed.id, user);
-					sent.push(user);
+					await database.sendFriendRequest(authed.id, uid);
+					sent.push(uid);
 				};
 			};
 		};
