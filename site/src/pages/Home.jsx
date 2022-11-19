b9185d65496a464affffb2a2bee40f7edffa4203
anc932 <anc932@mail.usask.ca>
2022-11-18 20:47:09 -0600

    home page added
    added styles and section to home page

--- site/src/pages/Home.jsx
+++ site/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Navigation } from "../components/Navigation";
+import logo from "../images/logo.svg";
+import friends from "../images/friends.png";
+import sharedgames from "../images/sharedgames.png";
 
 function Home() {
   return (
@@ -7,11 +10,70 @@ function Home() {
       <header>
         <Navigation />
       </header>
+      <body>
+        <div className="home-section grid-container">
+        <div className="grid-container">
+          <div className="grid-item grid-item1">
+              <h2>
+                Find Common Ground With Your Friends 
+              </h2>
+              <p className="p-home">convergence allows you to see what games you and your friends
+          all own to make finding games to play together easier!
+          </p>
+        <button className="get-started-button">Get Started</button>
+          </div>
+          <div className="grid-item">
+              <img src={logo} alt="logo" width="200"></img>
+          </div>
+        </div>
+        </div>
+
+        <div className="home-section-dark grid-container">
+        <div className="grid-container">
+          <div className="grid-item grid-item1">
+              <h3>
+                Large Game Group? No Problem! 
+              </h3>
+              <p className="p-home">convergence allows you to easily see what games you and your multiple friends
+          all own. Only playing games with one person? you got it! playing games with 10 
+           other people? Easily see what games everyone owns
+        </p>
+          </div>
+          <div className="grid-item">
+          <img src={sharedgames} alt="shared games"></img>
+          </div>
+        </div>
+        </div>
+
+        <div className="home-section grid-container">
+        <div className="grid-container">
+          <div className="grid-item grid-item1">
+          <img src={friends} alt="friends"></img>
+          </div>
+          <div className="grid-item">
+              <h3>
+                Automatically Add Games From Your Favourite Platforms!
+              </h3>
+              <p className="p-home">no one likes adding things manually whenever they dont have to, 
+          which is why convergence allows you to link some of your Favourite
+          platforms to the site. so that you can have large parts of your game 
+          library automatically added to the site!
+        </p>
+          </div>
+        </div>
+        </div>
+        {/* <div className="btn-center">  */}
+        <button className="get-started-button btn-center">Get Started</button>
+        {/* </div> */}
+        
       <main className="home">
-        <h1 className="heading">Convergence</h1>
+        
       </main>
+      </body>
     </>
   );
 }
 
 export default Home;
+
+
